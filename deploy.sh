74f761119cbdecf0713c5ebd5722891872c6993f
Your Name <you@example.com>
2015-09-10 16:01:09 +0200

    added app_name parameter

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 NEW_PROJECT=$1
 OSE_MASTER=$2
 GIT_URL=$3
+APP_NAME=$4
 #ssh -l alice 192.168.122.100 "oc login -u alice --insecure-skip-tls-verify --server=https://ose-master.rhpaas.com:8443"
 #ssh -l alice 192.168.122.100 "oc new-project $NEW_PROJECT"
 #ssh -l alice 192.168.122.100 "oc project $NEW_PROJECT"
@@ -10,5 +11,5 @@ GIT_URL=$3
 ssh -l alice 192.168.122.100 "oc login -u alice --insecure-skip-tls-verify --server=$OSE_MASTER"
 ssh -l alice 192.168.122.100 "oc new-project $NEW_PROJECT"
 ssh -l alice 192.168.122.100 "oc project $NEW_PROJECT"
-ssh -l alice 192.168.122.100 "oc new-app $GIT_URL"
+ssh -l alice 192.168.122.100 "oc new-app $GIT_URL --name=$APP_NAME"
 ssh -l alice 192.168.122.100 "oc expose service $APP_NAME --hostname=$APP_NAME.cloudapps.rhpaas.com
